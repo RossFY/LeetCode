049916ba659388ce213c729cbb42d8d6a74712d4
RossFY <yfang14@stevens.edu>
2019-02-24 20:28:30 -0500

    Reverse Integer

---
+++ src/Feb_24_2019/Exercise7.java
@@ -0,0 +1,39 @@
+package Feb_24_2019;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
+
+public class Exercise7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int x = Integer.parseInt(line);
+            
+            int ret = new Solution().reverse(x);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
